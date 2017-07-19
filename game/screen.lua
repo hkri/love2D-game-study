8985ab011604ad566cabbe215981c0203473408e
hkri <john_granturismo04@yahoo.com>
2017-07-20 01:56:09 +0800

    working on metatables.

--- game/screen.lua
+++ game/screen.lua
@@ -4,14 +4,18 @@
 local module = { }
 
 ---------- [Entities] ----------
-local head = {X = 0, Y = 0}
+local POINT = {X = 0, Y = 0}
+local head = setmetatable(POINT, {})
+head.X = 0
 
 ---------- [VARIABLES AND SETTINGS] ----------
 -- Resolution: 600x400
 -- Scale: 4 = 150 x 100
 local scale   = 4
 local maxlen  = 200
+local snake   = {}
 
+local _fDefault = love.graphics.newFont(12)
 
 ---------- [LOCAL FUNCTIONS] ----------
 
@@ -19,7 +23,7 @@ local maxlen  = 200
 ---------- [LOVE2D EVENTS] ----------
 
 function module.load()
-
+  head.Y = 30
 end
 
 function module.keypressed(key)
@@ -31,7 +35,9 @@ function module.update(dt)
 end
 
 function module.draw(g)
-
+  g.setFont(_fDefault)
+  g.print(head.X, 0, 0)
+  g.print(head.Y, 0, 20)
 end
 
 return module -- Export the module
