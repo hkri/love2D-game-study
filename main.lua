887b5a861787b7d89392957e717b490b90edc2c2
John Espiritu <john_granturismo04@yahoo.com>
2017-07-19 10:28:44 +0800

    Added game screen to load and handle events.

--- main.lua
+++ main.lua
@@ -6,33 +6,34 @@
 local gfx = love.graphics
 
 -- Prepare game screens.
-local main_menu = require('main-menu/screen')
+local main_menu   = require('main-menu/screen')
+local game_screen = require('game/screen')
 
 -- Score screens in an array
-local GAME_SCREEN = {main_menu}
-local GAME_STATE  = 1
+local GAME_SCREEN = {main_menu, game_screen}
+G_GAME_STATE  = 1
 
 -- Do basic loading here.
 function love.load()
-  GAME_SCREEN[GAME_STATE].load()
+  GAME_SCREEN[G_GAME_STATE].load()
 end
 
 -- Game update loop
 -- Perform game logic here.
 -- dt - Delta Time, time since last frame.
 function love.update(dt)
-  GAME_SCREEN[GAME_STATE].update(dt)
+  GAME_SCREEN[G_GAME_STATE].update(dt)
 end
 
 -- Keypressed handler
 -- Handle key pressed events here.
 -- key - the key that was pressed
 function love.keypressed(key)
-  GAME_SCREEN[GAME_STATE].keypressed(key)
+  GAME_SCREEN[G_GAME_STATE].keypressed(key)
 end
 
 -- Game draw/render loop
 -- Draw graphics here.
 function love.draw()
-  GAME_SCREEN[GAME_STATE].draw(gfx)
+  GAME_SCREEN[G_GAME_STATE].draw(gfx)
 end
