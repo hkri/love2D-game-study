423afb2e1a386624c67fb2e557de5ff5025ec661
John Espiritu <john_granturismo04@yahoo.com>
2017-07-19 10:28:57 +0800

    Added game screen.

---
+++ game/screen.lua
@@ -0,0 +1,37 @@
+-- The main game screen
+-- This is the screen where the game happens
+
+local module = { }
+
+---------- [Entities] ----------
+local head = {X = 0, Y = 0}
+
+---------- [VARIABLES AND SETTINGS] ----------
+-- Resolution: 600x400
+-- Scale: 4 = 150 x 100
+local scale   = 4
+local maxlen  = 200
+
+
+---------- [LOCAL FUNCTIONS] ----------
+
+
+---------- [LOVE2D EVENTS] ----------
+
+function module.load()
+
+end
+
+function module.keypressed(key)
+
+end
+
+function module.update(dt)
+
+end
+
+function module.draw(g)
+
+end
+
+return module -- Export the module
